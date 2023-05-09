3783e4ce5c4c3ea98b3f7f044f6b0f8199ff1d92
johnjos1 <johnjos1@students.zhaw.ch>
2023-05-09 21:15:17 +0200

    start

---
+++ src/main/java/johnjos1/zhaw/joshmdmtwo/JoshmdmtwoApplication.java
@@ -0,0 +1,13 @@
+package johnjos1.zhaw.joshmdmtwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoshmdmtwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoshmdmtwoApplication.class, args);
+	}
+
+}
