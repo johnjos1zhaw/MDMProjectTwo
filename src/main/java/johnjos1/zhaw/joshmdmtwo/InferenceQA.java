d6b9444a69feb35cd8d1087c4dc5629fe4c4db1b
johnjos1 <johnjos1@students.zhaw.ch>
2023-05-18 13:23:42 +0200

    back and frontend work

--- src/main/java/johnjos1/zhaw/joshmdmtwo/InferenceQA.java
+++ src/main/java/johnjos1/zhaw/joshmdmtwo/InferenceQA.java
@@ -6,9 +6,7 @@ import ai.djl.modality.nlp.qa.QAInput;
 import ai.djl.repository.zoo.Criteria;
 import ai.djl.repository.zoo.ZooModel;
 import ai.djl.training.util.ProgressBar;
-import ai.djl.translate.DeferredTranslatorFactory;
 import ai.djl.translate.TranslateException;
-import ai.djl.Application;
 
 import java.io.IOException;
 import java.nio.file.Paths;
@@ -18,13 +16,13 @@ import java.util.Scanner;
 public class InferenceQA {
     public static void main(String[] args) throws IOException, TranslateException, ModelException {
         try (Scanner scanner = new Scanner(System.in)) {
-            // Get paragraph from user
-            System.out.print("Enter a paragraph: ");
-            String paragraph = scanner.nextLine();
             // Get question from user
             System.out.print("Enter a question: ");
             String question = scanner.nextLine();
-            
+            // Get paragraph from user
+            System.out.print("Enter a paragraph: ");
+            String paragraph = scanner.nextLine();
+                       
             QAInput input = new QAInput(question, paragraph);
             String answer = InferenceQA.qa_predict(input);
             
@@ -39,9 +37,9 @@ public class InferenceQA {
                 .optModelPath(Paths.get("trace_cased_disitlbertqa.pt"))
                 .optTranslator(translator)
                 .optProgress(new ProgressBar()).build();
-                
-        ZooModel<QAInput, String> model = criteria.loadModel();
-        try (Predictor<QAInput, String> predictor = model.newPredictor(translator)) {
+    
+        try (ZooModel<QAInput, String> model = criteria.loadModel();
+             Predictor<QAInput, String> predictor = model.newPredictor(translator)) {
             return predictor.predict(input);
         }
     }
